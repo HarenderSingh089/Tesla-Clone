b941bbb112f7b066ddb05ec6937a3fc920b82b24
Harender Singh <singhharender089@gmail.com>
2021-11-09 00:55:02 +0530

    ADD: Section for Tesla

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,64 @@
 import React from 'react'
+import styled from 'styled-components'
+import Section from './Section';
 
 function Home() {
     return (
-        <div>
-            This is a home page
-        </div>
+        <Container >
+            <Section 
+            title="Model S" 
+            description="Order online For Touchless Delivery"
+            backgroundImg="/images/model-s.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+            />
+            <Section 
+            title="Model Y" 
+            description="Order online For Touchless Delivery"
+            backgroundImg="/images/model-y.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+            />
+            <Section 
+            title="Model 3" 
+            description="Order online For Touchless Delivery"
+            backgroundImg="/images/model-3.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+            />
+            <Section 
+            title="Model X" 
+            description="Order online For Touchless Delivery"
+            backgroundImg="/images/model-x.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+            />
+            <Section 
+            title="Lowest Cost Solar Panels in America" 
+            description="Money-back guarantee"
+            backgroundImg="/images/solar-panel.jpg"
+            leftBtnText="Order Now"
+            rightBtnText="Learn More"
+            />
+            <Section 
+            title="Solar for New Roofs" 
+            description="Solar Roof costs Less Than a New Roof plus Solar panels"
+            backgroundImg="/images/solar-roof.jpg"
+            leftBtnText="Order Now"
+            rightBtnText="Learn More"
+            />
+            <Section 
+            title="Accessories" 
+            description=""
+            backgroundImg="/images/accessories.jpg"
+            leftBtnText="shop Now"
+            />
+        </Container>
     )
 }
 
-export default Home
+export default Home;
+
+const Container = styled.div`
+
+`
---
+++ src/Components/Section.js
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+
+function Section({title, description, backgroundImg, leftBtnText, rightBtnText}) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <ItemText>
+        <h1>{ title }</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{ leftBtnText }</LeftButton>
+          {rightBtnText && 
+          <RightButton>{ rightBtnText }</RightButton>
+         }
+        </ButtonGroup>
+        <DownArrow src="./images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  );
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: ${props => `url(${props.bgImage})` };
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  text-align: center;
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width:768px){
+      flex-direction:column;
+  }
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  color: white;
+  border-radius: 100px;
+  height: 40px;
+  width: 256px;
+  text-align: center;
+  text-transform: uppercase;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0.85;
+  font-size: 12px;
+  margin:8px;
+  cursor: pointer;
+ 
+`;
+const RightButton = styled(LeftButton)`
+    background-color: white;
+    color: black;
+    opacity: 0.65;
+`;
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s ;
+`;
+
+const Buttons =styled.div`
+    
+`
